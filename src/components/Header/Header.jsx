51f2b96cf619db32dfc28072041d0d5c16f675b7
Your Name <your-email@example.com>
2024-11-24 02:18:46 +0300

    header part

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { BsSearch } from "react-icons/bs";
+import { SlLocationPin } from "react-icons/sl";
+import { BiCart } from "react-icons/bi";
+import classes from "./Header.module.css"
+import LowerHeader from '../LowerHeader/LowerHeader';
+const Header = () => {
+  return (
+    <>
+      <section>
+        <div className={classes.header_container}>
+          {/* logo section */}
+          <div className={classes.logo_container}>
+            <a href="#">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon log "
+              />
+            </a>
+            <div className={classes.delivery}>
+              {/* delivery */}
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Delivered to</p>
+                <span>Ethiopia</span>
+              </div>
+            </div>
+          </div>
+          {/* search section*/}
+          <div className={classes.search}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" id="" />
+            <BsSearch size={25} />
+          </div>
+
+          {/* other section */}
+
+          <div className={classes.order_container}>
+            <a href="" className={classes.language}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png"
+                alt=""
+              />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </a>
+            <a href="">
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </a>
+            <a href="">
+              <p>returns</p>
+              <span>& Orders</span>
+            </a>
+            <a href="" className={classes.cart}>
+              <BiCart size={35}/>
+              <span>0</span>
+            </a>
+          </div>
+        </div>
+          </section>
+          <LowerHeader/>
+    </>
+  );
+}
+
+export default Header
